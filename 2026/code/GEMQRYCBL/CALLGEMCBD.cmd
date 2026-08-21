cb0891e62dabe906553949cbdf6b6acea84e92fe
jwoehr <jwoehr@softwoehr.com>
2026-08-21 10:56:12 -0600

    feat: Bob reviews and fixes after review. Note: program does not exit correctly on F3

--- 2026/code/GEMQRYCBL/CALLGEMCBD.cmd
+++ 2026/code/GEMQRYCBL/CALLGEMCBD.cmd
@@ -3,5 +3,5 @@
              PARM       KWD(MODEL) TYPE(*CHAR) LEN(100) DFT('gemini-2.5-pro') +
                           PROMPT('Gemini model name')
 
-             PARM       KWD(QUERY) TYPE(*CHAR) LEN(500) DFT('What is IBM i?') +
+             PARM       KWD(QUERY) TYPE(*CHAR) LEN(5000) DFT('What is IBM i?') +
                           PROMPT('Query text')
